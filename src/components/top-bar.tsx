05be77bda99a0066585f8e090d3a6c6b884e308c
tamashiro-syuta <syuta5825@gmail.com>
2024-09-01 15:50:27 +0900

    🔥 不要なimportを削除

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -3,11 +3,9 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { APP_NAME } from "@/constants";
-import SwipeableTemporaryDrawer from "./drawer";
 import { Props } from "./app-bar";
 
 export default function TopBar({ open, toggleDrawer }: Props) {
