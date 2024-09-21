e26dab14bd88ffb83386ecf059641095a74a3c5b
tamashiro-syuta <syuta5825@gmail.com>
2024-09-21 16:27:24 +0900

    🐛 リンク先間違っていたから修正

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -2,10 +2,7 @@ const AppBar = () => {
   return (
     <nav className="bg-primary border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             TIL-Viewer
           </span>
